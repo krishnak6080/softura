7b43d80135ac47d755a9700919ab2baa7b6684df
krishna <88821325+krishnak6080@users.noreply.github.com>
2022-09-22 08:43:47 +0530

    add

---
+++ taskemployee.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KonsoleApp
+{
+    public class PersonalDetails
+    {
+        string name = "X";
+        char gender = ' ';
+        int yearss;
+        public void getdata()
+        {
+            Console.WriteLine("Enter name :");
+            name = Console.ReadLine();
+            Console.WriteLine("Enter gender :");
+            gender = Convert.ToChar(Console.Read());
+            Console.WriteLine("Enter experience :");
+            yearss = Convert.ToInt32(Console.ReadLine());
+        }
+        public void display()
+        {
+            Console.WriteLine("Name : " + name);
+            Console.WriteLine("Gender : " + gender);
+            Console.WriteLine("Experience : " + yearss);
+        }
+    }
+    public class Employee : PersonalDetails
+    {
+        int empID;
+        int salary;
+        string dept;
+        public void get()
+        {
+            getdata();
+            Console.WriteLine("Enter your employee ID :");
+            empID = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter your salary :");
+            salary = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter your department :");
+            dept = Console.ReadLine();
+        }
+        public void disp()
+        {
+            display();
+            Console.WriteLine("Employee ID : " + empID);
+            Console.WriteLine("Salary : " + salary);
+            Console.WriteLine("Department : " + dept);
+        }
+        public void calcSalary()
+        {
+            switch (dept.ToUpper())
+            {
+                case "SALES & MARKETING":
+                    salary = (salary * 120 / 100);
+                    break;
+                case "PRODUCTION":
+                    salary = (salary * 110 / 100);
+                    break;
+                default:
+                    Console.WriteLine("Error in calculating the salary...");
+                    break;
+            }
+        }
+    }
+    internal class taskemployee
+    {
+        public static void Main()
+        {
+            Employee e1 = new Employee();
+            Employee e2 = new Employee();
+            e1.get();
+            e2.get();
+        }
+    }
+}
---
+++ tasknameage.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KonsoleApp
+{
+    public class Details
+    {
+        public int age { get; set; }
+        public string name { get; set; }
+        public Details(int age, string name)
+        {
+            this.age = age;
+            this.name = name;
+        }
+        public Details(Details det)
+        {
+            age = det.age;
+            name = det.name;
+        }
+        public Details()
+        {
+            age = 21;
+            name = "Default";
+        }
+        public void disp()
+        {
+            Console.WriteLine("Name : {0}\nAge : {1}\n", name, age);
+        }
+    }
+    internal class tasknameage
+    {
+        public static void Main()
+        {
+            Details details = new Details(101, "tommy");
+            Details d1 = new Details(details);
+            Details d2 = new Details() { age = 21, name = "wilbur" };
+            details.disp();
+            d1.disp();
+            d2.disp();
+            Console.WriteLine("Done");
+        }
+    }
+}

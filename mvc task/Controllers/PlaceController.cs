6fe0246179e67b7c4c65be7b1a72a0b2dd1ac6a8
krishna <88821325+krishnak6080@users.noreply.github.com>
2022-10-01 13:14:01 +0530

    mvc task

---
+++ mvc task/Controllers/PlaceController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using placeApp.Models;
+
+namespace placeApp.Controllers
+{
+    public class PlaceController : Controller
+    {
+        public IActionResult GetPlace()
+        {
+            Places place = new Places() { num = 1, name = "Ibiza" };
+            return View("Places", place);
+        }
+
+    }
+}
---
+++ mvc task/Models/Places.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace placeApp.Models
+{
+    public class Places : Controller
+    {
+        public int num { get; set; }
+        public string name { get; set; }
+    }
+}

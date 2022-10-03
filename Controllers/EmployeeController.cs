3afb085ea1dd03a705b58df8ce977f2bea5f5eca
krishna <88821325+krishnak6080@users.noreply.github.com>
2022-10-03 15:59:28 +0530

    MVC Task 2

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using EmployeeApp.Models;
+
+namespace EmployeeApp.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult AllEmployee()
+        {
+            List<Employee> emps = Employee.GetAllEmployee();
+            return View(emps);
+        }
+        public IActionResult OneEmployee()
+        {
+            Employee emp = Employee.GetOneEmployee();
+            return View(emp);
+        }
+        public IActionResult InsertEmployee()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,37 @@
+﻿namespace EmployeeApp.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Gender { get; set; }
+
+        public static Employee GetOneEmployee()
+        {
+            Employee empl = new Employee()
+            {
+                Id = 1,
+                FirstName = "White",
+                LastName = "Walker",
+                Gender = "Male"
+            };
+            return empl;
+        }
+
+        public static List<Employee> GetAllEmployee()
+        {
+            List<Employee> employees = new()
+            {
+                new Employee(){Id = 1, FirstName = "Heisen", LastName="Berg", Gender="Male"},
+                new Employee(){Id = 2, FirstName = "Kris", LastName="Feck", Gender="Female"},
+                new Employee(){Id = 3, FirstName = "David", LastName="Off", Gender="Male"},
+                new Employee(){Id = 4, FirstName = "Ros", LastName="Benn", Gender="Female"},
+                new Employee(){Id = 5, FirstName = "Feb", LastName="Will", Gender="Female"},
+            };
+            return employees;
+        }
+    }
+}
